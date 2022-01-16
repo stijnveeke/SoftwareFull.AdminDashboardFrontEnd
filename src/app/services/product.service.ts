e77183e32b64867dd05bc67a2cb377a621dc7dbc
Stitch <st.van.eekelen@gmail.com>
2022-01-16 16:59:00 +0100

    Added live product endpoints

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
 
   getAllProducts()
   {
-    return this.authHttp.get<ProductOutput[]>('https://localhost:5021/api/Product');
+    return this.authHttp.get<ProductOutput[]>('https://softwarefullproductcomponent.azurewebsites.net/api/Products');
     // await this.authHttp.get<ProductOutput[]>('https://localhost:44366/api/Products').subscribe((products) => {
     //   console.log(products);
     //   productList = products as ProductOutput[];
@@ -23,27 +23,27 @@ export class ProductService {
 
   getProduct(productSlug: string): Observable<ProductOutput>
   {
-    return this.authHttp.get<ProductOutput>(`https://localhost:5021/api/Product/${productSlug}`, {
+    return this.authHttp.get<ProductOutput>(`https://softwarefullproductcomponent.azurewebsites.net/api/Products/${productSlug}`, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.auth.getAccessToken())
     });
   }
 
   createProduct(data: ProductInput) {
-    this.authHttp.post('https://localhost:5021/api/Product', data, {
+    this.authHttp.post('https://softwarefullproductcomponent.azurewebsites.net/api/Products', data, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.auth.getAccessToken())
     }).subscribe((d) => console.log(d), (err) => console.log(err));
     console.log(data);
   }
 
   editProduct(productId: number, data: ProductInput) {
-    this.authHttp.put(`https://localhost:5021/api/Product/${productId}`, data, {
+    this.authHttp.put(`https://softwarefullproductcomponent.azurewebsites.net/api/Products/${productId}`, data, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.auth.getAccessToken())
     }).subscribe((d) => console.log(d), (err) => console.log(err));
     console.log(data);
   }
 
   deleteProduct(productId: number) {
-    this.authHttp.delete(`https://localhost:5021/api/Product/${productId}`, {
+    this.authHttp.delete(`https://softwarefullproductcomponent.azurewebsites.net/api/Products/${productId}`, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.auth.getAccessToken())
     }).subscribe((d) => console.log(d), (err) => console.log(err));;
   }
